6f82d8b863c12345dd37857efb9c83519e4113a0
AndresCerero <andycega07@gmail.com>
2023-06-28 21:31:10 -0500

    Deploy V3

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Pages/App/Index'
+import App from './Pages/App/Index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
